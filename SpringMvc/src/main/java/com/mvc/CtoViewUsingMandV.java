f181a2aa55db6325570b6c0ad353169b3902f134
SudhakarBadigar <sudhakar.badigar87@gmail.com>
2023-08-29 19:16:26 +0530

    spring mvc

--- SpringMvc/src/main/java/com/mvc/CtoViewUsingMandV.java
+++ SpringMvc/src/main/java/com/mvc/CtoViewUsingMandV.java
@@ -1,32 +1,34 @@
 package com.mvc;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/ctvController")
 public class CtoViewUsingMandV {
 	
-	@RequestMapping("/window")
+	@RequestMapping(path="/window" ,method = RequestMethod.GET)
 	public ModelAndView employee() {
 		
 		ModelAndView mav = new ModelAndView();
 		mav.addObject("company", "Accenture");
 		
 		mav.addObject("company-code", "ISO-8089");
+		LocalDateTime now = LocalDateTime.now();
 		
 		List<String> emp=new ArrayList<String>();
 		emp.add("rama");
 		emp.add("krishna");
 		emp.add("shiva");
-		
+		mav.addObject("date",now);
 		mav.addObject("Employees", emp);
-		
-		
-		
+		System.out.println(now);
 		System.out.println("model and view");
 		mav.setViewName("front1");
 		return mav;
--- SpringMvc/src/main/webapp/WEB-INF/views/front1.jsp
+++ SpringMvc/src/main/webapp/WEB-INF/views/front1.jsp
@@ -1,6 +1,8 @@
+<%@page import="java.time.LocalDateTime"%>
 <%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@page isELIgnored="false"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,7 +18,6 @@
    String comp=(String)request.getAttribute("company");
 	String comcode=(String) request.getAttribute("company-code");
 	List<String> employee = (List<String>) request.getAttribute("Employees");
-   
 	
 	//String name=(String)request.getAttribute("name");
 	//Integer id = (Integer)request.getAttribute("id");
@@ -24,9 +25,17 @@
       
 %>
 
+  <%-- 	LocalDateTime datenow  = (LocalDateTime)request.getAttribute("date"); --%>
 <h1>Company name is  <%=comp %></h1>
 <h1>Company code is <%=comcode %></h1>
 	<h1>Employees in the company: </h1>
+	<%-- <h1>current Date is --><%=datenow %></h1> --%>
+	
+	<!-- This is JSTL ie jsp standard tag library -->
+	<h1> Date is --> 
+	${date}
+	</h1>
+	
 	<%
 	
      	for(String emp :employee)
