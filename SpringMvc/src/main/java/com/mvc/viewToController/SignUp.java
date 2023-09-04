b7bbebcfcff6a92f1b6125f66e3099bc05e86fa0
SudhakarBadigar <sudhakar.badigar87@gmail.com>
2023-09-04 19:28:32 +0530

    spring mvc

--- SpringMvc/src/main/java/com/mvc/viewToController/SignUp.java
+++ SpringMvc/src/main/java/com/mvc/viewToController/SignUp.java
@@ -1,14 +1,46 @@
 package com.mvc.viewToController;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SignUp {
-
+	
+	//process controller
 	@RequestMapping("/contact")
 	public String contact(){
 		
 		return "signup";
+	}
+
+	/* in case of servlet we used":- getparameter("name of the field");
+	@RequestMapping(path = "/process", method = RequestMethod.POST)
+	public String handleFormData( HttpServletRequest req) {
+		String pass = req.getParameter("userPassword");
+		System.out.println("password is  " + pass);
+		return "success";
+	}*/
+	
+	//Handler controller which will specify the final destination of signup.jsp
+	@RequestMapping(path = "/process", method = RequestMethod.POST)
+	public String handleFormData(
+			@RequestParam("userName") String name,
+			@RequestParam("userPassword") String pass,
+			@RequestParam("userEmail") String email,
+			Model model) {
+		System.out.println("username is " + name);
+		System.out.println("useremail is " + email);
+		System.out.println("userpassword is " + pass);
+		
+		model.addAttribute("un",name);
+		model.addAttribute("ue", email);
+		model.addAttribute("up", pass);
+		
+		return "success";
 	}
 }
---
+++ SpringMvc/src/main/webapp/WEB-INF/views/success.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+    <%@ page isELIgnored="false" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body style="background-color: yellow; text-align:center;">
+<h1>Congrats Login is successfull..</h1>
+<h1>Congrats all your data has been recorded</h1>
+
+<h1>username is :  ${un}</h1>
+<h1>email is : ${ue}</h1>
+<h1>password is : ${up}</h1>
+
+
+</body>
+</html>
